76c2efb03b1a9f049f4b0450145dd417cc215686
Richard McCollister <kraki5525@gmail.com>
2018-11-26 08:04:25 -0500

    Start vue implementation

--- MyCasts.Web/wwwroot/js/site.js
+++ MyCasts.Web/wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿// Write your Javascript code.
+﻿import Vue from 'vue';
+import App from './App'
+
+var app = new Vue({
+    el: '#app',
+    components: { App },
+    template: '<App/>'
+    // data: {
+    //     podcasts:[],
+    //     name: "",
+    //     url: ""
+    // },
+    // methods: {
+    //     add: function(event) {
+    //         this.podcasts.push({name: this.name, url: this.url});
+    //         this.name = "";
+    //         this.url = "";
+    //     }
+    // }
+});
